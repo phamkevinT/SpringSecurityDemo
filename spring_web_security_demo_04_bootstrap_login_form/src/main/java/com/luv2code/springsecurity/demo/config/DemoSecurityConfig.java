e4fb09131d4fae23135a14a9eb8c3e9c4e8d04bc
Kevin Pham <phambamkevin@gmail.com>
2021-03-06 13:35:21 -0800

    bootstrap login css

---
+++ spring_web_security_demo_04_bootstrap_login_form/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -0,0 +1,43 @@
+package com.luv2code.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		// Add user for in memory authentication (just for demo, will implement with database later)
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		
+		auth.inMemoryAuthentication().withUser(users.username("john").password("test123").roles("EMPLOYEE"));
+		auth.inMemoryAuthentication().withUser(users.username("mary").password("test123").roles("MANAGER"));
+		auth.inMemoryAuthentication().withUser(users.username("susan").password("test123").roles("ADMIN"));
+		
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests()
+				.anyRequest().authenticated() // Require all request to be authenticated
+			.and()
+			.formLogin()
+				.loginPage("/showMyLoginPage")	// Show our custom login form at this request mapping
+				.loginProcessingUrl("/authenticateTheUser")	// Login form should POST data to this URL for processing
+				.permitAll(); // Allow everyone to see the login form.
+	}
+	
+	
+	
+
+	
+}
